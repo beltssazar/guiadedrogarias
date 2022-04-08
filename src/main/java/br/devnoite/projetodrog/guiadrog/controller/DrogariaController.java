f82c7e787f7878a24cca466d0997fc6a369e3d72
Daniel Fernando <dfernando1296@gmail.com>
2022-04-07 22:33:36 -0300

    Terceiro commit. Adicionado o input do tipo file, iniciada a lõgica de upload do arquivo no Firebase, com criação de chave privada e lógica para alteração do nome do arquivo.

--- src/main/java/br/devnoite/projetodrog/guiadrog/controller/DrogariaController.java
+++ src/main/java/br/devnoite/projetodrog/guiadrog/controller/DrogariaController.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import br.devnoite.projetodrog.guiadrog.model.Drogaria;
+import br.devnoite.projetodrog.guiadrog.repository.DrogariaRepository;
 import br.devnoite.projetodrog.guiadrog.repository.SegRepository;
 
 @Controller
@@ -13,9 +17,22 @@ public class DrogariaController {
 	@Autowired
 	private SegRepository drogTipo;
 	
+	@Autowired
+	private DrogariaRepository repository;
+	
 	@RequestMapping("cadDrog")
 	public String form(Model model) {
 		model.addAttribute("tipos", drogTipo.findAllByOrderByOpcaoAsc());
 		return "cad_drog";
+		
+	}
+	
+	@RequestMapping("salvaDrog")
+	public String salvar(Drogaria drogaria, @RequestParam("fileFotos") MultipartFile[] fileFotos) {
+		System.out.println(fileFotos.length);
+		//repository.save(drogaria);
+		return "redirect:cadDrog";
 	}
 }
+
+
